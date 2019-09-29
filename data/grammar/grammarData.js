bcd85df23487db970e18bb7716c2c74c019c09d7
nhok2106 <khailt.cntt@gmail.com>
2019-09-29 21:09:13 +0700

    update data grammar

--- data/grammar/grammarData.js
+++ data/grammar/grammarData.js
@@ -64,7 +64,174 @@ const data = {
             +   "- E.g: He didn’t go fishing yesterday. (Ngày hôm qua anh ấy đã không đi câu cá.)\n"
             +   "  Did you go to the library last week? (Tuần trước bạn có đến thư viện không?)\n"
                   }]
-            }
+        },
+        {
+            "title": "Present Continuous",
+            "description": "Thì hiện tại tiếp diễn",
+            "data": [{
+                "content": 
+                "1. Định nghĩa thì hiện tại tiếp diễn\n"
+            +   ""
+            +   "Thì hiện tại tiếp diễn dùng để diễn tả những sự việc xảy ra ngay lúc chúng ta nói hay xung quanh thời điểm nói, và hành động chưa chấm dứt\n"
+            +   ""
+            +   "2. Cách dùng của thì hiện tại tiếp diễn\n"
+            +   "- Diễn đạt một hành động đang xảy ra tại thời điểm nói\n"
+            +   "I am eating my lunch right now. (Bây giờ tôi đang ăn trưa)\n"
+            +   "- Diễn tả một hành động hoặc sự việc nói chung đang diễn ra nhưng không nhất thiết phải thực sự diễn ra ngay lúc nói.\n"
+            +   "I’m quite busy these days. I’m doing my assignment.(Dạo này tôi khá là bận. Tôi đang làm luận án)\n"
+            +   "- Diễn đạt một hành động sắp xảy ra trong tương lai gần. Thường diễn tả một kế hoạch đã lên lịch sẵn\n"
+            +   "I bought the ticket yesterday. I am flying to New York tomorrow (tôi đã mua vé máy bay. Tôi sẽ bay sang New York ngày mai))\n"
+            +   "Hành động thường xuyên lặp đi lặp lại gây sự bực mình hay khó chịu cho người nói. Cách dùng này được dùng với trạng từ “always, continually”\n"
+            +   ""
+            +   "3. Công thức thì hiện tại tiếp diễn\n"
+            +   "1. Khẳng định:"
+            +   "S + am/ is/ are + V-ing\n"
+            +   "Trong đó: S (subject): Chủ ngữ\n"
+            +   "am/ is/ are: là 3 dạng của động từ “to be”\n"
+            +   "V-ing: là động từ thêm “–ing”"
+            +   "CHÚ Ý:\n"
+            +   "– S = I + am\n"            
+            +   "– S = He/ She/ It + is\n"            
+            +   "– S = We/ You/ They + are\n"
+            +   "2. Phủ định:\n"
+            +   "S + am/ is/ are + not + V-ing\n"
+            +   "3. Câu hỏi:\n"
+            +   "Am/ Is/ Are + S + V-ing?\n"
+            +   "Đối với câu hỏi ta chỉ việc đảo động từ “to be” lên trước chủ ngữ.\n"
+            +   "5. Dấu hiệu nhận biết thì hiện tại tiếp diễn"
+            +   "Trong câu có các trạng từ chỉ thời gian:\n"
+            +   "– Now: bây giờ – Right now: Ngay bây giờ – At the moment: lúc này – At present: hiện tại – At + giờ cụ thể (at 12 o’clock)\n"           
+            +   "Trong câu có các động từ như:\n"
+            +   "– Look! (Nhìn kìa!) – Listen! (Hãy nghe này!) – Keep silent! (Hãy im lặng)\n"
+            }]
+        },
+        {
+            "title": "Past Continuous",
+            "description": "Thì quá khứ tiếp diễn",
+            "data": [{
+                "content": 
+                "1. Định nghĩa thì quá khứ tiếp diễn\n"
+            +   "Thì quá khứ tiếp diễn (Past Continuous Tense) dùng khi muốn nhấn mạnh diễn biến hay quá trình của sự vật hay sự việc hoặc thời gian sự vật hay sự việc đó diễn ra …\n"
+            +   "2. Cách dùng thì quá khứ tiếp diễn\n"
+            +   "- Diễn đạt hành động đang xảy ra tại một thời điểm trong quá khứ\n"
+            +   "When my sister got there, it was 8 p.m\n"
+            +   "- Diễn đạt hai hành động xảy ra đồng thời trong quá khứ\n"
+            +   "While I was taking a bath, she was using the computer.\n"
+            +   "- Diễn đạt hành động đang xảy ra thì có hành động khác xen vào\n"
+            +   "I was walking in the street when I suddenly fell over.\n"
+            +   "I was listening to the news when she phoned.\n"
+            +   "- Hành động lặp đi lặp lại trong quá khứ và làm phiền đến người khác\n"
+            +   "3. Công thức thì quá khứ tiếp diễn\n"
+            +   "- Câu khẳng định\n"
+            +   "S + was/were + V-ing (+ O)\n"
+            +   "I was thinking about him last night.\n"
+            +   "- Câu phủ định\n"
+            +   "S + was/were + not + V-ing (+ O)\n"
+            +   "I wasn’t thinking about him last night.\n"
+            +   "- Câu nghi vấn\n"
+            +   "(Từ để hỏi) + was/were + S + V-ing (+ O)?\n"
+            +   "Were you thinking about him last night?\n"
+            +   "4. Dấu hiệu nhận biết thì quá khứ tiếp diễn\n"
+            +   "Trong câu có các trạng từ chỉ thời gian trong quá khứ kèm theo thời điểm xác định.\n"
+            +   "– at + giờ + thời gian trong quá khứ (at 12 o’clock last night,…)\n"            
+            +   "– at this time + thời gian trong quá khứ. (at this time two weeks ago, …)\n"            
+            +   "– in + năm (in 2000, in 2005)\n"            
+            +   "– in the past (trong quá khứ)\n"            
+            +   "- Trong câu có “when” khi diễn tả một hành động đang xảy ra và một hành động khác xen vào.\n"
+            +   "Ngoài việc dựa vào cách dùng của thì Quá khứ tiếp diễn, ngữ cảnh của câu thì các bạn cũng hãy cân nhắc sử dụng thì Quá khứ tiếp diễn khi có xuất hiện các từ như: While (trong khi); When (Khi); at that time (vào thời điểm đó);…\n"
+            }]
+        },
+        {
+            "title": "PRESENT PERFECT",
+            "description": "Thì hiện tại hoàn thành",
+            "data": [{
+                "content": 
+                "1. Định nghĩa\n"
+            +   "Diễn tả một hành động diễn ra trong quá khứ và vẫn còn tiếp diễn tới hiện tại\n"
+            +   "2. Cách dùng\n"
+            +   "- Diễn tả hành động diễn ra trong quá khứ và vẫn còn tiếp diễn đến hiện tại\n"
+            +   " I have lived in Ha Noi for 5 years.\n"
+            +   "- Diễn tả hành động đã hoàn thành tính đến thời điểm hiện tại nhưng không rõ thời gian kết thúc.\n"
+            +   "She has written three books and now she is working on the fourth one.\n"
+            +   "- Diễn tả sự kiện quan trọng trong đời.\n"
+            +   "It is a happy party that I have came in my life\n"
+            +   "- Dùng để chỉ kết quả\n"
+            +   "I have finished my homework.\n"
+            +   "- Diễn tả kinh nghiệm, trải nghiệm tính đến thời điểm hiện tại.\n"
+            +   "Have you ever been to American?\n"
+            +   "3. Công thức\n"
+            +   "- Khẳng định\n"
+            +   "S+ have/has+ V3\n"
+            +   "I have cooked for 2 hours.\n"
+            +   "- Phủ định\n"
+            +   "S+ have/has not + V3\n"
+            +   "She hasn’t met her mother since April.\n"
+            +   "Nghi vấn\n"
+            +   "Have/has + S + V3\n"
+            +   "Have you arrived in American yet?\n"
+            +   "4. Dấu hiệu nhận biết\n"
+            +   "Những từ xuất hiện trong thì này như Just = Recently = Lately, Already, Before, Ever, Never, For, Since, Yet, So far = Until now = Up to now = Up to the present.\n"
+            }]
+        },
+        {
+            "title": "PRESENT PREFECT CONTINUOUS",
+            "description": "Thì hiện tại hoàn thành tiếp diễn",
+            "data": [{
+                "content": 
+                "1. Định nghĩa\n"
+            +   "Diễn tả một hành động đã bắt đầu trong quá khứ và diễn ra cho đến hiện tại. Hành động có tính liên tục\n"
+            +   "2. Cách dùng\n"
+            +   "Được dùng diễn tả một hành động đã bắt đầu trong quá khứ và diễn ra cho đến hiện tại. Hành động có tính liên tục.\n"
+            +   "I have been walking on this street for more than 1 hours."
+            +   "Thì HTHTTD nhấn mạnh vào quá trình còn thì HTHT thì nhấn mạnh vào kết quả, các bạn lưu ý nhé.\n"
+            +   "Lưu ý, theo cách dùng các thì trong tiếng Anh, thì HTHTTD cũng tương tự các thì tiếp diễn, sẽ không dùng được với các động từ chỉ trạng thái, cảm xúc, động từ không mang tính kéo dài, khi chỉ số lượng, số lần.\n"
+            +   "3. Công thức\n"
+            +   "- Khẳng định\n"
+            +   "S + have/has been + V-ing + O\n"
+            +   "I have been going to school\n"
+            +   "- Phủ định\n"
+            +   "S+ hasn’t/ haven’t+ been+ V-ing + O\n"
+            +   "They haven’t been working together.\n"
+            +   "Nghi vấn\n"
+            +   "Has/have+ S+ been+ V-ing + O\n"
+            +   "Have you been traveling HCM City?\n"
+            +   "4. Dấu hiệu nhận biết\n"
+            +   "- Đó là all ( day, week…), since, for – lưu ý cách dùng ở trên cho thì hiện tại hoàn thành. Một số từ khác: recently, lately, in the past, in recent years, up until now, and so far.\n"
+            +   " How long: bao lâu\n"
+            +   "Since + mốc thời gian. Ex: since 2000\n"
+            +   "For + khoảng thời gian Ex: for 3 years\n"
+            }]
+        },
+        {
+            "title": "PAST PERPECT",
+            "description": "Thì quá khứ hoàn thành",
+            "data": [{
+                "content": 
+                "1. Định nghĩa\n"
+            +   "Được dùng để diễn tả một hành động diễn ra trước một hành động khác trong quá khứ. Hành động sau là ở thì quá khứ đơn. Bạn có thể hình dung theo thứ tự là quá khứ hoàn thành – quá khứ đơn – hiện tại đơn nhé.\n"
+            +   "2. Cách dùng\n"
+            +   "- Diễn tả một hành động diễn ra trước một hành động khác trong quá khứ\n"
+            +   "We had had lunch before went to school\n"
+            +   "- Một hành động diễn ra trước một thời điểm khác trong quá khứ\n"
+            +   "When my father checked, I had finished my homework.\n"
+            +   "- Hành động xảy ra để dẫn đến hành động khác.\n"
+            +   "He had lost 10kg and became a handsome man.\n"
+            +   "- Diễn tả điều kiện không có thực trong câu điều kiện loại 3\n"
+            +   "If we had been invited, we would have come to her party.\n"
+            +   "3. Công thức\n"
+            +   "- Khẳng định\n"
+            +   "S + had + Vpp\n"
+            +   "- Phủ định\n"
+            +   "S +had not + Vpp\n"
+            +   "- Nghi vấn\n"
+            +   "Had + S + Vpp?\n"
+            +   "4. Dấu hiệu nhận biết\n"
+            +   "Sử dụng thì quá khứ hoàn thành thường đi kèm với thì quá khứ đơn. Các từ thường xuất hiện: before, after, Until then, by the time, prior to that time, for, as soon as, by, ...When, when by, by the time, by the end of + time in the past …\n"
+            +   "Lưu ý sử dụng :\n"            
+            +   "- QKHT + before + QKD\n"           
+            +   "- After + QKHT, QKĐ.\n"
+            }]
+        }
 
     ]}
 const getGrammarData = () => {
